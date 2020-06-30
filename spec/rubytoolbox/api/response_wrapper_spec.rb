7946f8a7cc89c876850018a8974875d004a63880
Christoph Olszowka <christoph@olszowka.de>
2020-06-30 22:05:45 +0200

    Allow to cast wrapped objects back to hashes

--- spec/rubytoolbox/api/response_wrapper_spec.rb
+++ spec/rubytoolbox/api/response_wrapper_spec.rb
@@ -3,9 +3,23 @@
 require "spec_helper"
 
 RSpec.describe Rubytoolbox::Api::ResponseWrapper do
+  sub = Class.new(Rubytoolbox::Api::ResponseWrapper) do
+    field :key
+  end
+
   sample = Class.new(described_class) do
     field :foo
     field :bar, &:reverse
+
+    field :sub do |input|
+      sub.new input
+    end
+
+    field :subcollection do |inputs|
+      inputs.map do |input|
+        sub.new(input)
+      end
+    end
   end
 
   it "wraps given data as attributes of the class" do
@@ -17,5 +31,25 @@ RSpec.describe Rubytoolbox::Api::ResponseWrapper do
 
   it "correctly handles false value when given as string key" do
     expect(sample.new("foo" => false, bar: "123").foo).to be == false
+  end
+
+  it "can be converted back to a hash" do # rubocop:disable RSpec/ExampleLength
+    object = sample.new(
+      foo: "foo",
+      bar: "bar",
+      sub: { key: "value" },
+      subcollection: [{ key: "value" }]
+    )
+
+    expect(object.to_h).to be == {
+      "foo" => "foo",
+      "bar" => "rab",
+      "sub" => {
+        "key" => "value",
+      },
+      "subcollection" => [
+        { "key" => "value" },
+      ],
+    }
   end
 end
